3001c10387f33f92a47741b00d14cf2eb89c0d4d
ydunga <55959066+ydunga@users.noreply.github.com>
2021-03-24 18:03:42 +0000

    Add files via upload

---
+++ diversity.js
@@ -0,0 +1,20 @@
+anime ({
+    targets: '.whittaker',
+    translateX: 70,
+    delay:anime.stagger(500),
+    loop: true
+  })
+  
+  anime ({
+    targets: '.classify',
+    translateX: 70,
+    delay:anime.stagger(500),
+    loop: true
+  })
+  
+  anime ({
+    targets: '.humanorder',
+    translateX: 70,
+    delay: anime.stagger(500),
+    loop: true
+  })
